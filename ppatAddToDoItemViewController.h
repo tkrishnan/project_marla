8787644e8de3bcdcb509b67b7f717c0842bdab61
Anuhya Ghorakavi <anuhya.ghorakavi@gmail.com>
2013-11-24 18:43:56 -0500

    hopefully files are working now

---
+++ ppatAddToDoItemViewController.h
@@ -0,0 +1,16 @@
+//
+//  ppatAddToDoItemViewController.h
+//  PPAT
+//
+//  Created by Anuhya Ghorakavi on 11/24/13.
+//  Copyright (c) 2013 Anuhya Ghorakavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ppatToDoItem.h"
+
+@interface ppatAddToDoItemViewController : UIViewController
+
+@property ppatToDoItem *toDoItem;
+
+@end
---
+++ ppatAddToDoItemViewController.m
@@ -0,0 +1,50 @@
+//
+//  ppatAddToDoItemViewController.m
+//  PPAT
+//
+//  Created by Anuhya Ghorakavi on 11/24/13.
+//  Copyright (c) 2013 Anuhya Ghorakavi. All rights reserved.
+//
+
+#import "ppatAddToDoItemViewController.h"
+
+@interface ppatAddToDoItemViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *doneButton;
+
+@end
+
+@implementation ppatAddToDoItemViewController
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if (sender != self.doneButton) return;
+    if (self.textField.text.length > 0) {
+        self.toDoItem = [[ppatToDoItem alloc] init];
+        self.toDoItem.itemName = self.textField.text;
+        self.toDoItem.completed = NO;
+    }
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
